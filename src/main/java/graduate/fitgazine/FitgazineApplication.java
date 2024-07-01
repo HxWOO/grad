07ff5408b4d99ef5095431189f0a6501ae085efb
HyunWoo <kimjd766@gmail.com>
2024-07-01 13:36:37 +0900

    프로젝트 세팅

---
+++ src/main/java/graduate/fitgazine/FitgazineApplication.java
@@ -0,0 +1,13 @@
+package graduate.fitgazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitgazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitgazineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/graduate/fitgazine/FitgazineApplicationTests.java
@@ -0,0 +1,13 @@
+package graduate.fitgazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitgazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
